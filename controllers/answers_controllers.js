076a447f347ebbc183db15cc29d693013107b600
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-11-26 19:32:29 +0530

    Routes and controller done

--- controllers/answers_controllers.js
+++ controllers/answers_controllers.js
@@ -1,5 +1,25 @@
 const crypto = require('crypto');
 const mysqlConnection = require('../connection');
 module.exports.create = async function(req, res){
-    
- }
+
+    let sql = 'INSERT INTO answer SET ?';
+            let post = {Answer_id:crypto.randomBytes(64).toString('hex'),
+                        Question_id:req.body.post,
+                        Roll_Number:req.user.rollnum,
+                        Content:req.body.content,
+                        Creation_datetime:new Date()
+            };
+            mysqlConnection.query(sql,post,async function(err,result){
+                if(err){
+                    console.log('answer not created :: Error :',err);
+                    return;
+                }else{
+                    console.log(result);
+                    req.flash('success','Question posted');
+                    return res.redirect('back');
+                    
+                }
+
+            });
+
+}
--- controllers/questions_controller.js
+++ controllers/questions_controller.js
@@ -2,4 +2,23 @@ const crypto = require('crypto');
 const mysqlConnection = require('../connection');
 module.exports.create = async function(req, res){
 
+    let sql = 'INSERT INTO question SET ?';
+            let post = {Question_id:crypto.randomBytes(64).toString('hex'),
+                        Roll_Number:req.user.rollnum,
+                        Content:req.body.content,
+                        Creation_datetime:new Date()
+            };
+            mysqlConnection.query(sql,post,async function(err,result){
+                if(err){
+                    console.log('question not created :: Error :',err);
+                    return;
+                }else{
+                    console.log(result);
+                    req.flash('success','Question posted');
+                    return res.redirect('back');
+                    
+                }
+
+            });
+
 }
