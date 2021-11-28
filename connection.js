8ed2bdf08841f35d73c00e8374259a32ca83250c
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-11-28 14:06:31 +0530

    minor changes

--- connection.js
+++ connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 var mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "25032000Ka@",
-    database: "qawebapp",
+    password: "password",
+    database: "DBMS_QUERY_WEBAPP",
     multipleStatements: true,
 });
 mysqlConnection.connect(function(err) {
